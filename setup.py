f68ad693c3a848d84478210e11d1d8273d852fca
manin <yefansulayman@gmail.com>
2023-01-17 23:53:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 with open('README.md') as f:
     readme = f.read()
-with open('requirements/build.txt') as f:
+with open('requirements/build.txt',encoding='utf-8') as f:
     requirements = f.read()
 setup(
     # Metadata
